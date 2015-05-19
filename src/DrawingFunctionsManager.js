73080273bb7beffa36af3147a0cd0bf2f4b2f97f
bendem <online@bendem.be>
2015-05-19 13:38:09 +0200

    Use chained calls to draw (following JsGameLib)

--- src/DrawingFunctionsManager.js
+++ src/DrawingFunctionsManager.js
@@ -26,120 +26,124 @@ DrawingFunctionsManager.prototype = {
 
     registerMonster: function(renderer) {
         game.renderer.register('monster_health', function(ctx, info) {
-            ctx.beginPath();
-            ctx.fillStyle = 'rgba(0,255,0,0.8)';
-            ctx.strokeStyle = 'rgba(0,0,0,0.9)';
-            ctx.rect(info.x, info.y, info.width, info.height);
-            ctx.stroke();
-            ctx.fill();
+            ctx
+                .beginPath()
+                .set('fillStyle', 'rgba(0,255,0,0.8)')
+                .set('strokeStyle', 'rgba(0,0,0,0.9)')
+                .rect(info.x, info.y, info.width, info.height)
+                .stroke()
+                .fill();
         });
         game.renderer.register('monster_missing_health', function(ctx, info) {
-            ctx.beginPath();
-            ctx.fillStyle = 'rgba(255,0,0,0.8)';
-            ctx.rect(info.x, info.y, info.width, info.height);
-            ctx.fill();
+            ctx
+                .beginPath()
+                .set('fillStyle', 'rgba(255,0,0,0.8)')
+                .rect(info.x, info.y, info.width, info.height)
+                .fill();
         });
         game.renderer.register('monster_body', function(ctx, info) {
-            ctx.beginPath();
-            ctx.fillStyle = 'rgba(0,0,0,0.7)';
-            ctx.rect(info.x, info.y, info.width, info.height);
-            ctx.fill();
+            ctx
+                .beginPath()
+                .set('fillStyle', 'rgba(0,0,0,0.7)')
+                .rect(info.x, info.y, info.width, info.height)
+                .fill();
         });
     },
 
     registerTower: function(renderer) {
         renderer.register('tower', function(ctx, info) {
-            ctx.beginPath();
-            Draw.roundedRect(
-                ctx,
-                info.position,
-                info.width,
-                info.height,
-                5
-            );
-
-            ctx.fillStyle = 'rgba(50,50,150,0.8)';
-            ctx.fill();
+            ctx
+                .beginPath()
+                .roundedRect(
+                    info.position,
+                    info.width,
+                    info.height,
+                    5
+                )
+                .set('fillStyle', 'rgba(50,50,150,0.8)')
+                .fill();
             this.blur(ctx).stroke();
         }, this);
 
         renderer.register('tower_range', function(ctx, info) {
-            ctx.beginPath();
-            Draw.circle(
-                ctx,
-                info.position,
-                info.range
-            );
+            ctx
+                .beginPath()
+                .circle(
+                    info.position,
+                    info.range
+                );
             this.blur(ctx).stroke();
         }, this);
     },
 
     registerBullet: function(renderer) {
         game.renderer.register('bullet', function(ctx, info) {
-            ctx.beginPath();
-            Draw.circle(ctx, info.position, info.radius);
+            ctx
+                .beginPath()
+                .circle(info.position, info.radius)
 
-            ctx.fillStyle = 'rgba(255, 100, 80, 0.9)';
-            ctx.fill();
+                .set('fillStyle', 'rgba(255, 100, 80, 0.9)')
+                .fill()
 
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = '#333';
-            ctx.stroke();
+                .set('lineWidth', 2)
+                .set('strokeStyle', '#333')
+                .stroke();
         });
     },
 
     registerLifeCounter: function(renderer) {
         renderer.register('life_counter', function(ctx, info) {
-            ctx.font = info.counter.size + 'px sans-serif';
-            ctx.textBaseline = 'middle';
-            ctx.textAlign = 'center';
+            ctx
+                .set('font', info.counter.size + 'px sans-serif')
+                .set('textBaseline', 'middle')
+                .set('textAlign', 'center');
             var width = ctx.measureText(info.counter.count).width;
 
-            ctx.fillStyle = 'rgba(0,0,0,0.5)';
-            ctx.strokeStyle = '#111';
-            ctx.beginPath();
-            Draw.roundedRect(
-                ctx,
-                info.counter.position,
-                info.counter.hPad * 2 + width, info.counter.vPad * 2 + info.counter.size,
-                3
-            );
-            ctx.fill();
-            ctx.stroke();
+            ctx
+                .set('fillStyle', 'rgba(0,0,0,0.5)')
+                .set('strokeStyle', '#111')
+                .beginPath()
+                .roundedRect(
+                    info.counter.position,
+                    info.counter.hPad * 2 + width, info.counter.vPad * 2 + info.counter.size,
+                    3
+                )
+                .fill()
+                .stroke()
 
-            ctx.fillStyle = '#eee';
-            ctx.fillText(
-                info.counter.count,
-                info.counter.position.x + info.counter.hPad + width / 2,
-                info.counter.position.y + info.counter.vPad + info.counter.size / 2
-            );
+                .set('fillStyle', '#eee')
+                .fillText(
+                    info.counter.count,
+                    info.counter.position.x + info.counter.hPad + width / 2,
+                    info.counter.position.y + info.counter.vPad + info.counter.size / 2
+                );
         });
     },
 
     registerSelection: function(renderer) {
         renderer.register('selection', function(ctx, info) {
-            ctx.beginPath();
-            Draw.roundedRect(
-                ctx,
-                info.position,
-                square_dimension.x,
-                square_dimension.y,
-                6
-            );
-            ctx.strokeStyle = '#eee';
-            ctx.lineWidth = 2;
-            ctx.shadowBlur = 1;
-            ctx.shadowColor = '#111';
-            ctx.stroke();
+            ctx
+                .beginPath()
+                .roundedRect(
+                    info.position,
+                    square_dimension.x,
+                    square_dimension.y,
+                    6
+                )
+                .set('strokeStyle', '#eee')
+                .set('lineWidth', 2)
+                .set('shadowBlur', 1)
+                .set('shadowColor', '#111')
+                .stroke();
         });
     },
 
     blur: function(ctx) {
-        ctx.lineWidth = 3;
-        ctx.shadowBlur = 5;
-        ctx.strokeStyle = '#fff';
-        ctx.shadowColor = '#111';
-        return ctx;
+        return ctx
+            .set('lineWidth', 3)
+            .set('shadowBlur', 5)
+            .set('strokeStyle', '#fff')
+            .set('shadowColor', '#111');
     },
 
 };
